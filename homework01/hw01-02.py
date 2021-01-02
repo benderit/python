2327472406b4c3f52176bf7f13691230a17dcd5b
benderit <37499409+benderit@users.noreply.github.com>
2021-01-02 19:31:02 +0300

    Add files via upload

--- homework01/hw01-02.py
+++ homework01/hw01-02.py
@@ -1,10 +1,4 @@
 #2. Пользователь вводит время в секундах. Переведите время в часы, минуты и секунды и выведите в формате чч:мм:сс. Используйте форматирование строк.
-def str_time_format(int):
-    if(int < 10):
-        return '0'+str(int)
-    else:
-        return str(int)
-
 print("Привет, эта программа конвертирует секунды в часовой формат HH:MM:SS\n")
 a=int(input("Введите число секунд для преобразования: "))
 
@@ -18,7 +12,7 @@ M=int((a-H*3600)/60)
 S=(a-H*3600-M*60)
 
 if (days == 0):
-    print(str_time_format(H)+":"+str_time_format(M)+":"+str_time_format(S))
+    print(f"{H:02}:{M:02}:{S:02}")
 else:
-    print(str(days)+" days, "+str_time_format(H)+":"+str_time_format(M)+":"+str_time_format(S))
+    print(f"{days} days, {H:02}:{M:02}:{S:02}")
 
